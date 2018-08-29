a01fc41075888a4e534058d9c4eb3d2c744c3263
Roger Shao <s9755059@gmail.com>
2018-08-29 12:20:33 +0800

    comment transform.py

--- COCO2YOLO/transform.py
+++ COCO2YOLO/transform.py
@@ -1,5 +1,8 @@
 import json
+#NOTICE! different dataset may need different setting 
 
+
+#Remeber to Change ClassName and ClassNum pair to the coco json file
 className = {
 1:'Gastropoda',
 2:'Osteroida',
@@ -16,53 +19,38 @@ className = {
 
 classNum = [1,2,3,4,5,6,7,8,9,10,11]
 
+#store file
 def writeNum(Num):
     with open("COCO_train.json","a+") as f:
         f.write(str(Num))
 
-# with open("instances_val2014.json","r+") as f:
-#     data = json.load(f)
-    # annData = data["annotations"]
-    # print(annData[0])
-    # for x in annData[0]:
-    #     if(x == "image_id"):
-    #         print(type(x))
-    #         print(x+ ":" + str(annData[0][x]))
-    #     if (x == "image_id" or x == "bbox" or x == "category_id"):
-    #         print(x + ":" + annData[0][x])
-    #     if (x == "image_id" or x == "bbox" or x == "category_id"):
-    #         print(x+ ":" + annData[0][x])
-
-# with open("test.json","w") as f:
-#     json.dump(annData, f, ensure_ascii=False)
-
 inputfile = []
 inner = {}
-##向test.json文件写入内容
+##write content to json file
 with open("/media/share33/Database/NOAA Fish Finding/Training Data release/annotations/habcam_seq0_training.mscoco.json","r+") as f:
     allData = json.load(f)
-    data = allData["annotations"]
-    image = allData['images']
-    print(data[1])
-    print(len(data))
+    data = allData["annotations"] #annotation part
+    image = allData['images'] #images part
+    print(data[1])  #check 
+    print(len(data))  #check
     print("read ready")
 
 for i in data:
-    if(i['category_id'] in classNum and (i['roi_shape'] == "bbox" or i['roi_shape']=='line')):
+    if(i['category_id'] in classNum and (i['roi_shape'] == "bbox" or i['roi_shape']=='line')): #We use only bbox and line(bbox) annotation
         for j in image:
             if(j["has_annots"]==True):
-                if(i["image_id"]==j["id"]):
+                if(i["image_id"]==j["id"]): #check if there are id both in annotation part and images part
                     inner = {
-                    "filename": str(j["file_name"]).zfill(6),
+                    "filename": str(j["file_name"]).zfill(6),     #file_name is the same,zfill is in case of trouble
                     "name": className[i["category_id"]],
                     "bndbox":i["bbox"]
                     }
                     inputfile.append(inner)
-print(type(inputfile))
-print(inputfile[0])
+print(type(inputfile)) #check
+print(inputfile[0])  #check
 #with open("COCO_train.json","a+") as f:
 #    for hostdict in inputfile:
 #        json.dump(hostdict,f)
 #        f.write("\n")
 inputfile = json.dumps(inputfile)
-writeNum(inputfile)
+writeNum(inputfile)   #outputfile
